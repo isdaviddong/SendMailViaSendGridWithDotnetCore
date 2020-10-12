37bf856de0d564b143d1a58edae1af2e9105196b
David <twdeveloper@hotmail.com>
2020-10-12 14:03:04 +0800

    v1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Threading;
+using System.ComponentModel;
+
+namespace dotnetCoreSendMail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //send mail with SendGrid
+            var server = "smtp.sendgrid.net";
+            var pwd = "SG.4aXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXlrECI"; //API secret
+            var sender = "xxxxxxxxx@hotmail.com"; //必須驗證過的發信者
+            SmtpClient client = new SmtpClient(server);
+            client.Port = 25;
+            client.Credentials = new System.Net.NetworkCredential("apikey", pwd);
+            client.Send(sender, "david@isrock.com.tw", "主旨", "內文");
+        }
+    }
+}
